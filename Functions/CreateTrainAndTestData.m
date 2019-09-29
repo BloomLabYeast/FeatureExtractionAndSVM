98ccc9a3e3f7b67f373b5a05e55a9a948484a120
Ayush Doshi <ayushdoshi1@gmail.com>
2019-09-28 21:39:32 -0400

    Updated documentation of code

--- Functions/CreateTrainAndTestData.m
+++ Functions/CreateTrainAndTestData.m
@@ -1,4 +1,8 @@
 function [trainingdata, traininglabels, testingdata, testinglabels] = CreateTrainAndTestData(array, string, PFT)
+
+% CREATETRAINANDTESTDATA is a function that is used to create training and
+% testing sets from one sample pool, following the percentage provided.
+
 matrixselected = datasample(1:size(array,1),round(size(array,1)*PFT/100),'Replace',false);
 trainingdata = array(matrixselected,:);
 testingdata = 1:size(array,1);
